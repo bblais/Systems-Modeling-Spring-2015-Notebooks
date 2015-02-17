20c541aadee6c8821b61f9429ef2986d4e268a58
Brian Blais <bblais@gmail.com>
2015-02-17 10:31:45 -0500

    added sim with data

--- Tuesday Feb 17 2015.py
+++ Tuesday Feb 17 2015.py
@@ -23,6 +23,12 @@ t_data=data['day']
 y_data=data['height (cm)']
 plot(t_data,y_data,'o')
 
+# <markdowncell>
+
+# ## Linear growth example
+# 
+# $y' = {\rm constant}$
+
 # <codecell>
 
 y=2
@@ -36,13 +42,51 @@ dt=0.01
 reset()
 
 store(t,y)
-for i in range(1000):
+for i in range(9000):
     dy=constant*dt
     
     y=y+dy
     t=t+dt
     
-    store(t,y)
+    if i%100 == 0 :  # if the remainder with i/100 = 0 (i.e. i is divisible by 100)
+        store(t,y)
+
+# <codecell>
+
+t,y=recall()
+
+# <codecell>
+
+plot(t,y)
+plot(t_data,y_data,'o')
+
+# <markdowncell>
+
+# ## Exponential Growth
+# 
+# $y'=my$  where $m$ is a growth constant
+
+# <codecell>
+
+y=2
+t=0
+
+constant=.06
+dt=0.01
+
+# <codecell>
+
+reset()
+
+store(t,y)
+for i in range(9000):
+    dy=constant*y*dt
+    
+    y=y+dy
+    t=t+dt
+    
+    if i%100 == 0 :  # if the remainder with i/100 = 0 (i.e. i is divisible by 100)
+        store(t,y)
 
 # <codecell>
 
